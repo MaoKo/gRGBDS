2f2c14c7b3fcf331bdf11db657a8809e52e74466
MaoKo <Alexandre.Audibert@outlook.fr>
2019-11-04 22:50:47 +0100

    expand irps.inc

--- main_test.asm
+++ main_test.asm
@@ -1,4 +1,25 @@
 
-include "gRGBDS.inc"
+;include "gRGBDS.inc"
 
-printt "TEST\n"
+;end _catch_line
+
+;_replace_bracket_operator result, 1
+
+;match _, 4
+;_
+;end match
+
+;display "TEST", $A
+
+;_catch_line
+;irp A, 1
+;    %
+;end irpv
+
+include "irps.inc"
+include "preprocessor.inc"
+
+_replace_bracket_operator result, a<
+match _, result
+    _
+end match
