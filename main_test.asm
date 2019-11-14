71c2e5dae2039317186cf2583027393a91fe542c
MaoKo <Alexandre.Audibert@outlook.fr>
2019-11-14 20:58:58 +0100

    add somme optimization

--- main_test.asm
+++ main_test.asm
@@ -2,12 +2,23 @@
 ;include "gRGBDS.inc"
 
 include "pairing.inc"
+include "assert.inc"
+
 include "irps.inc"
 include "string.inc"
-include "assert.inc"
-include "preprocessor.inc"
+
+;include "preprocessor.inc"
+;include "sections.inc"
+;include "rpn.inc"
+
+include "fasmg_identifier.inc"
 
 repeat $10000
-    irps abc[+ident], A B C
-    end irps
+_iterate_string A, "DEADxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+end _iterate_string
 end repeat
+
+;repeat $10000
+;    irps item[+space|+ident], A B C D E F G R A CX FG E Z R C _ _ _ _ _ _ 
+;    end irps
+;end repeat
