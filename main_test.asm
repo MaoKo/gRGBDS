49bdb248ae87ec2d5b619087088530b7f394a2c4
MaoKo <Alexandre.Audibert@outlook.fr>
2019-11-19 23:36:37 +0100

    .

--- main_test.asm
+++ main_test.asm
@@ -5,7 +5,8 @@ include "pairing.inc"
 include "assert.inc"
 include "insert_token.inc"
 
-include "irps.inc"
+;include "irps.inc"
+include "irps_struc.inc"
 include "string.inc"
 
 include "preprocessor.inc"
@@ -14,9 +15,10 @@ include "preprocessor.inc"
 
 include "fasmg_identifier.inc"
 
-
 repeat $10000
-    irps item, _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
-    end irps
+irps A[+space], _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+end irpv
+end match
 end repeat
+
 
