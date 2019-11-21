d10803c001ffaf5fe2008e2c83fc6e39eea1cbc7
MaoKo <Alexandre.Audibert@outlook.fr>
2019-11-21 19:02:00 +0100

    .

--- main_test.asm
+++ main_test.asm
@@ -1,27 +1,36 @@
 
 ;include "gRGBDS.inc"
 
-include "pairing.inc"
-include "assert.inc"
-include "insert_token.inc"
+;include "pairing.inc"
+;include "assert.inc"
+;include "insert_token.inc"
 
 ;include "irps.inc"
-include "irps_struc.inc"
-include "string.inc"
+;include "irps_struc.inc"
+;include "string.inc"
 
-include "preprocessor.inc"
+;include "preprocessor.inc"
 ;include "sections.inc"
 ;include "rpn.inc"
 
-include "fasmg_identifier.inc"
+;include "fasmg_identifier.inc"
 
 ;repeat $10000
-;irps A, _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+;irps A[+space], _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
 ;end irpv
 ;end match
 ;end repeat
 
-define empty 
-irps A[+space], A B C empty
-    display "A = ", `A, $A
-end irps
+macro inst?
+end macro
+
+define A 1
+
+match __A, A
+    repeat $80000
+        if (__A)
+        end if
+        if (__A)
+        end if
+    end repeat
+end match
