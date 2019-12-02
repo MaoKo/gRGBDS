62976340edc0ebe793cb8debe8a13b469ec4fa13
MaoKo <Alexandre.Audibert@outlook.fr>
2019-12-02 06:53:30 +0100

    .

--- e.asm
+++ e.asm
@@ -1,46 +1,82 @@
+
+_irps_line? := $00
+namespace _irps_line?
+end namespace
+
 macro irps?! parameter?*, text?*&
-    local buffer, token
+    local buffer, active, tokens, finals
     buffer reequ text
-    while $01
-        match __input?, buffer
-            redefine __prefix?
-            redefine __prefix?.__input __delimiter__
-            match,__prefix
-                rawmatch __suffix?, __input
-                    match _1 =__delimiter__? _2, __prefix?.__suffix
-                        local shift, tname
-                        shift = lengthof(`__input) - lengthof(`_2) 
-                        tname = string ((($01 shl (shift * $08)) - $01) and (`__suffix))
-                        eval "define token ", tname
-                        eval "restore __prefix?.", tname
+    active = $00
+    irpv _, buffer
+        repeat $01, i:(`_)
+            if (~(definite _irps_line.i.active))
+                restore _irps_line.i.active
+                define  _irps_line.i.active
+                _irps_line.i.active = $00
+                define _irps_line.i tokens
+                active = $01
+            end if
+            finals equ _irps_line.i
+        end repeat
+    end irpv
+    if (active)
+        while $01
+            match __input?, buffer
+                redefine __prefix?
+                redefine __prefix?.__input __delimiter__
+                match,__prefix
+                    match _1 =__delimiter__? _2, __prefix?.__input
+                        local shift, input
+                        input = (`__input)
+                        shift = lengthof(input) - lengthof(`_2) 
+                        eval "match _, ", string ((($01 shl (shift * $08)) - $01) and input)
+                            define tokens _
+                            restore __prefix?._
+                        eval "end match"
                         redefine buffer _2
-                    else match name, __input
-                        define token name
-                        restore __prefix?.name
+                    else match _, __input
+                        define tokens _
+                        restore __prefix?._
                         break
                     end match
-                end rawmatch
-            else
-                rawmatch first remain, __input
-                    define token first
-                    redefine buffer remain
                 else
-                    rawmatch remain, __input
-                        define token remain
+                    rawmatch _ __remain, __input
+                        define tokens _
+                        redefine buffer __remain
+                    else
+                        define tokens __input
+                        break
                     end rawmatch
-                    break
-                end rawmatch
+                end match
+            else
+                define tokens
             end match
-        end match
-   end while
-    outscope irpv parameter, token
+        end while
+    end if
+    match _, finals
+        outscope irpv parameter, _
 end macro
 
 macro end?.irps!
-    end irpv
+        end irpv
+    end match
 end macro
 
-rept $10000
-irps A, A.C .L A.A.A.A.A.A.A.A.A.A.A.A.A.A.A.A.A.A. O+1 + A.A A.A A.A A.A A.A A.A A.A A.A A.A _ _ _ _ _ _ _ _ _ _ _ 
+rept $2
+    display "RESTART", $A
+irps A,_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+    display "A = ", `A, $A
 end irps
 end rept
+
+;line equ A + 4
+;match __line, line
+;    redefine __abc?
+;    redefine __abc?.__line |
+;    match,__abc?
+;        match _1 =| _2, __abc?.__line
+;            =_2
+;        end match
+;    end match
+;end match
+
