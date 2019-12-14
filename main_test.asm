14503d0aa7bef97717ae9a9235db68ae100f9453
MaoKo <Alexandre.Audibert@outlook.fr>
2019-12-14 15:30:21 +0100

    .

--- main_test.asm
+++ main_test.asm
@@ -10,14 +10,26 @@ include "irps.inc"
 
 include "preprocessor.inc"
 include "rpn.inc"
+include "evaluate_argument.inc"
 include "string_function.inc"
+include "fasmg_identifier.inc"
 ;include "sections.inc"
 ;include "rpn.inc"
 
-result _expand_single_line_macro\
-    abc strlen ("ABC")
+include "reverse.inc"
+include "forward.inc"
+include "itoa_base.inc"
+include "error.inc"
 
-match _, result
-_
-end match
+A = 5
+_assert_string A
+
+;    display "I = ", `i, $A
+
+;result _expand_single_line_macro\
+;    abc strlen (4)
+;
+;match _, result
+;_
+;end match
 
