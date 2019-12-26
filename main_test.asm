d86b316dbc0a2b81ff2ffad450d03cf110f25c6e
MaoKo <Alexandre.Audibert@outlook.fr>
2019-12-26 23:21:11 +0100

    .

---
+++ main_test.asm
@@ -0,0 +1,35 @@
+
+;include "gRGBDS.inc"
+
+include "pairing.inc"
+include "assert.inc"
+include "insert_token.inc"
+
+include "irps.inc"
+;include "string.inc"
+
+include "preprocessor.inc"
+include "rpn.inc"
+include "string_function.inc"
+include "fasmg_identifier.inc"
+;include "sections.inc"
+;include "rpn.inc"
+
+include "reverse.inc"
+include "forward.inc"
+include "itoa_base.inc"
+include "error.inc"
+
+include "bound_limit.inc"
+include "gbz80.inc"
+
+push AF
+pop DE
+
+;result _expand_single_line_macro\
+;    abc strlen (4)
+;
+;match _, result
+;_
+;end match
+
